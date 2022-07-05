20ef0323345d3ba3d17619ceaa9e09c08d44e31c
michealsource <kelvinmicheal209.com>
2022-07-05 15:19:23 -0700

    updated button

--- src/components/testimonial/TestmonialCarousel.js
+++ src/components/testimonial/TestmonialCarousel.js
@@ -5,12 +5,12 @@ function TestmonialCarousel() {
         <div className="testmonial-wrapper">
         <Carousel indicators={false}>
           <Carousel.Item>
-             <p style={{color:"#fff",fontSize:"14px",fontWeight:"600", lineHeight:"25px"}}>Jiffix actually exceeded my expectation. Super impressed with the service. They kept to time, gave me the right diagnosis and recommendation and it worked. The feel also very decent. I felt i got more than i paid for. Very professional and efficient. I think i found the perfect company for my cars. Thanks Jiffix</p>
+             <p style={{color:"#fff", height:"180px", fontSize:"14px",fontWeight:"600", lineHeight:"25px"}}>Jiffix actually exceeded my expectation. Super impressed with the service. They kept to time, gave me the right diagnosis and recommendation and it worked. The feel also very decent. I felt i got more than i paid for. Very professional and efficient. I think i found the perfect company for my cars. Thanks Jiffix</p>
              <p style={{color:"#c4c4c4"}}>Timi Olatunji</p>
           </Carousel.Item>
 
           <Carousel.Item>
-             <p style={{color:"#fff",fontSize:"14px",fontWeight:"600", lineHeight:"25px"}}>Jiffix's expertise is second to none. They were able to arrive at mine in record time and expertly fixed my automobile. I recommend their service.</p>
+             <p style={{height:"180px", color:"#fff",fontSize:"14px",fontWeight:"600", lineHeight:"25px"}}>Jiffix's expertise is second to none. They were able to arrive at mine in record time and expertly fixed my automobile. I recommend their service.</p>
              <p style={{color:"#c4c4c4"}}>Michael Ojo</p>
           </Carousel.Item>
 
